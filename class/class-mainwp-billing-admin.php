03a5de0c97ec386b53e63f35c17abadeccda7346
sflwa <philip@southfloridawebadvisors.com>
2025-11-08 17:53:26 -0500

    Update and rename class-mainwp-development-admin.php to class-mainwp-billing-admin.php

--- class/class-mainwp-billing-admin.php
+++ class/class-mainwp-billing-admin.php
@@ -1,20 +1,20 @@
 <?php
 /**
- * MainWP Development Admin
+ * MainWP Billing Admin
  *
  * This class handles the extension process.
  *
  * @package MainWP/Extensions
  */
 
- namespace MainWP\Extensions\Development;
+ namespace MainWP\Extensions\Billing;
 
  /**
-  * Class MainWP_Development_Admin
+  * Class MainWP_Billing_Admin
   *
   * @package MainWP/Extensions
   */
-class MainWP_Development_Admin {
+class MainWP_Billing_Admin {
 
 	public static $instance = null;
 	public $version         = '1.6';
@@ -27,7 +27,7 @@ class MainWP_Development_Admin {
 	}
 
 	/**
-	 * MainWP_Development_Admin constructor.
+	 * MainWP_Billing_Admin constructor.
 	 *
 	 * @return void
 	 */
@@ -38,10 +38,10 @@ class MainWP_Development_Admin {
 		add_action( 'mainwp_delete_site', array( &$this, 'hook_delete_site' ), 10, 1 );
 		add_action( 'admin_enqueue_scripts', array( &$this, 'admin_enqueue_scripts' ) );
 
-		MainWP_Development_DB::get_instance()->install();
-		MainWP_Development_Ajax::get_instance();
-		MainWP_Development_Overview::get_instance();
-		MainWP_Development_Individual::get_instance();
+		MainWP_Billing_DB::get_instance()->install();
+		MainWP_Billing_Ajax::get_instance();
+		MainWP_Billing_Overview::get_instance();
+		MainWP_Billing_Individual::get_instance();
 	}
 
 	/**
@@ -50,7 +50,7 @@ class MainWP_Development_Admin {
 	 * @return void
 	 */
 	public function localization() {
-		load_plugin_textdomain( 'mainwp-development-extension', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+		load_plugin_textdomain( 'mainwp-billing-extension', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 	}
 
 	/**
@@ -62,7 +62,7 @@ class MainWP_Development_Admin {
 	 * @return mixed Array of plugin meta data.
 	 */
 	public function plugin_row_meta( $plugin_meta, $plugin_file ) {
-		if ( 'mainwp-development-extension/mainwp-development-extension.php' != $plugin_file ) {
+		if ( 'mainwp-billing-extension/mainwp-billing-extension.php' != $plugin_file ) {
 			return $plugin_meta;
 		}
 		$slug     = basename( $plugin_file, '.php' );
@@ -80,9 +80,9 @@ class MainWP_Development_Admin {
 	 * @return void
 	 */
 	public function admin_enqueue_scripts() {
-		if ( isset( $_GET['page'] ) && ( 'Extensions-Mainwp-Development-Extension' === $_GET['page'] || 'ManageSitesDevelopmentIndividual' === $_GET['page'] || ( 'managesites' === $_GET['page'] && isset( $_GET['dashboard'] ) ) ) ) {
-			wp_enqueue_style( 'mainwp-development-extension', MAINWP_DEVELOPMENT_PLUGIN_URL . 'css/mainwp-development-extension.css', array(), $this->version );
-			wp_enqueue_script( 'mainwp-development-extension', MAINWP_DEVELOPMENT_PLUGIN_URL . 'js/mainwp-development-extension.js', array(), $this->version, true );
+		if ( isset( $_GET['page'] ) && ( 'Extensions-Mainwp-Billing-Extension' === $_GET['page'] || 'ManageSitesBillingIndividual' === $_GET['page'] || ( 'managesites' === $_GET['page'] && isset( $_GET['dashboard'] ) ) ) ) {
+			wp_enqueue_style( 'mainwp-billing-extension', MAINWP_BILLING_PLUGIN_URL . 'css/mainwp-billing-extension.css', array(), $this->version );
+			wp_enqueue_script( 'mainwp-billing-extension', MAINWP_BILLING_PLUGIN_URL . 'js/mainwp-billing-extension.js', array(), $this->version, true );
 		}
 	}
 
@@ -94,7 +94,7 @@ class MainWP_Development_Admin {
 	 * @return array $input Input.
 	 */
 	public function widgets_screen_options( $input ) {
-		$input['advanced-development-widget'] = __( 'Development Widget', 'mainwp-development-extension' );
+		$input['advanced-billing-widget'] = __( 'Billing Widget', 'mainwp-billing-extension' );
 		return $input;
 	}
 }
